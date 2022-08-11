d001b43dee62e23fcd67e607d1c36144594e55df
Alejandra Lezcano <alezcanosalinas@gmail.com>
2022-08-11 13:52:19 -0400

    Creamos y subimos archivos del repositorio

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { AddCategory,GifGrid} from "./Components";
+
+export const GifExpertApp = () => {
+    const [categories,setcategories]=useState(['Stranger Things']);
+    const onAddCategory=(newCategory)=>{
+        // console.log(newCategory);
+        if(categories.includes(newCategory)) return;
+
+        //se puede tambien poner todas las categorias en lowercase para evitar que se repita
+        setcategories([newCategory,...categories]);
+    }
+    // console.log(categories);
+  return (
+    <div>
+      <>
+        <h1>GifExpertApp</h1>
+
+        <AddCategory 
+            onNewCategory={(value)=>onAddCategory(value)}
+        />
+        {
+            categories.map((category)=>(
+                <GifGrid 
+                    key={category} 
+                    category={category}/>
+            ))
+        }
+
+      </>
+    </div>
+  )
+}
+
